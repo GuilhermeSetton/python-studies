078d8349cb6218d06ed2e45fd416702eac4228a7
GuilhermeSetton <guilherme.scbittencourt@gmail.com>
2025-08-20 12:00:43 -0300

    feat: Adiciona primeiro script de estudos em Python

---
+++ Dia 20 de agosto.py
@@ -0,0 +1,36 @@
+#####Dia 20/08 
+shit = 2
+text = "Hello World"
+print(shit)
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+alphabet.find('e')
+print(type(shit))
+print(type(alphabet))
+index = alphabet.find(text[0].lower())
+"""
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+index = alphabet.find(text[0].lower())
+print(index)
+shifted = alphabet[index + shift]
+print(shifted)
+"""
+"""
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+for char in text.lower():
+    index = alphabet.find(char)
+    print(char, index)
+    new_index = index + shift
+"""
+#Strings são imutáveis
+#text = 'Hello World'
+#shift = 3
+#alphabet = 'abcdefghijklmnopqrstuvwxyz'
+#
+#for char in text.lower():
+ #   index = alphabet.find(char)
+  #  new_index = index + shift
+   # new_char = alphabet[new_index]
+    #print('char:', char, 'new char:', new_char)
